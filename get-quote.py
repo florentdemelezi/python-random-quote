0139c54128f9b8ab3dad62ed5f1c28ccb41b1642
Florent <f.demelezi@gmail.com>
2021-12-17 20:40:20 +0100

    Update on get-quote.py
    Florent

--- get-quote.py
+++ get-quote.py
@@ -1,4 +1,4 @@
-def main():
+def primary():
   print("Keep it logically awesome.")
 
   #f = open("quotes.txt")
@@ -8,5 +8,5 @@ def main():
   #print(quotes)
 
 if __name__== "__main__":
-  main()
+  primary()
 
