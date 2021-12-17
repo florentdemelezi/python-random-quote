6467564699616df24d6db77f313af745326f89f9
Florent <f.demelezi@gmail.com>
2021-12-17 21:00:20 +0100

    Update get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -1,6 +1,8 @@
+import random
 def primary():
   print("Keep it logically awesome.")
-
+last 13
+rnd = random.randint(0, last)
   f = open("C:/Users/hqrpg0/Documents/GitHub/python-random-quote/quotes.txt")
   quotes = f.readlines()
   f.close()
